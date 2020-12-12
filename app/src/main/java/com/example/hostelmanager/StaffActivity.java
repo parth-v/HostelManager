70d488d9a429829c386f649ef959fb6b6d2d2439
Parth_V <parthvashisht20@gmail.com>
2020-12-13 01:52:06 +0530

    Staff and admin functionalities added

--- app/src/main/java/com/example/hostelmanager/StaffActivity.java
+++ app/src/main/java/com/example/hostelmanager/StaffActivity.java
@@ -2,13 +2,32 @@ package com.example.hostelmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class StaffActivity extends AppCompatActivity {
-
+    EditText nameT, rollT, deptT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff);
+    }
+
+    public void onSubmit(View view) {
+
+        String name = nameT.getText().toString();
+        String roll = rollT.getText().toString();
+        String dept = deptT.getText().toString();
+
+        if(name.length()==0||roll.length()==0||dept.length()==0) {
+            Toast.makeText(this, "Please provide all the details!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        String data = "New Staff Added\nName: " + name + "\nID: " + roll + "\nDept: " + dept;
+        Toast.makeText(this, data, Toast.LENGTH_LONG).show();
     }
 }
