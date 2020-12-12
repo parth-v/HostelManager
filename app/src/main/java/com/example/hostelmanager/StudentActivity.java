d31b0b378dfe6e1d9f615e923e07e3b9ae9fbf23
Parth_V <parthvashisht20@gmail.com>
2020-12-13 01:57:08 +0530

    Student basic functionality added

--- app/src/main/java/com/example/hostelmanager/StudentActivity.java
+++ app/src/main/java/com/example/hostelmanager/StudentActivity.java
@@ -3,12 +3,32 @@ package com.example.hostelmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class StudentActivity extends AppCompatActivity {
+    EditText nameT, rollT, deptT, yearT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
     }
+
+    public void onSubmit(View view) {
+        String name = nameT.getText().toString();
+        String roll = rollT.getText().toString();
+        String dept = deptT.getText().toString();
+        String year = yearT.getText().toString();
+
+        if(name.length()==0||roll.length()==0||dept.length()==0||year.length()==0) {
+            Toast.makeText(this, "Please provide all the details!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        String data = "New Student Added\nName: " + name + "\nID: " + roll + "\nDept: " + dept +"\nYear: " + year;
+        Toast.makeText(this, data, Toast.LENGTH_LONG).show();
+    }
+
 }
