74777b45831e363e6e1e3ecebf4ac7d5e84704f6
Parth_V <parthvashisht20@gmail.com>
2020-12-14 01:30:16 +0530

    Staff and student details activity addedd

---
+++ app/src/main/java/com/example/hostelmanager/StudentDetails.java
@@ -0,0 +1,18 @@
+package com.example.hostelmanager;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StudentDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_details);
+
+    }
+}
