e1355bf7d94e89be1358bf376d922423bb2492df
Parth_V <parthvashisht20@gmail.com>
2020-12-14 01:35:36 +0530

    detail screen functionalities added

--- app/src/main/java/com/example/hostelmanager/StaffDetails.java
+++ app/src/main/java/com/example/hostelmanager/StaffDetails.java
@@ -3,16 +3,26 @@ package com.example.hostelmanager;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StaffDetails extends AppCompatActivity {
 
+    TextView nameT, rollT, deptT;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_details);
 
+        nameT = (TextView) findViewById(R.id.Name);
+        rollT = (TextView) findViewById(R.id.RollNo);
+        deptT = (TextView) findViewById(R.id.Dept);
+    }
+
+    public void EditDetails(View view) {
+        //open edit screen
     }
 }
--- app/src/main/java/com/example/hostelmanager/StudentDetails.java
+++ app/src/main/java/com/example/hostelmanager/StudentDetails.java
@@ -3,16 +3,24 @@ package com.example.hostelmanager;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StudentDetails extends AppCompatActivity {
-
+    TextView nameT, rollT, deptT, yearT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_details);
+        nameT = (TextView) findViewById(R.id.Name);
+        rollT = (TextView) findViewById(R.id.RollNo);
+        deptT = (TextView) findViewById(R.id.Dept);
+        yearT = (TextView) findViewById(R.id.Year);
+    }
 
+    public void EditDetails(View view) {
+        //open edit screen
     }
 }
